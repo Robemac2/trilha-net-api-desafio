5ae5f73f84dab7b84b3b0023ae676f112992b956
Roberto Macedo <robertoneto5@hotmail.com>
2024-04-19 19:08:20 -0300

    Criado endpoint ObterTodos

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -31,8 +31,14 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("ObterTodos")]
         public IActionResult ObterTodos()
         {
-            // TODO: Buscar todas as tarefas no banco utilizando o EF
-            return Ok();
+            var tarefas = _context.Tarefas;
+
+            if (tarefas == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tarefas);
         }
 
         [HttpGet("ObterPorTitulo")]
